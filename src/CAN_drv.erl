6af7dcc99b3416de3af53968b477c98bde40a8f0
ddobroczynski <ddobroczynski@d384496a-9eda-4eb4-9579-66ba5c7a03b1>
2010-11-24 12:15:19 +0000

    documentation
    git-svn-id: svn+ssh://vcs.lpue.cs.put.poznan.pl/srv/svn/leonardi/trunk/erleos@143 d384496a-9eda-4eb4-9579-66ba5c7a03b1

--- src/CAN_drv.erl
+++ src/CAN_drv.erl
@@ -7,17 +7,19 @@
 %%%         <li>opening a CAN port in read/write mode (underneath using POSIX
 %%%            `open(...)' function);</li>
 %%%         <li>receiving CAN frames returning `raw' binary or list of Erlang objects
-%%%             describing the frames contents;</li>
-%%%         <li>sending a list of frames through the CAN port;</li>
-%%%         <li>closing the port.</li>
+%%%             describing the frames contents (uses POSIX `pselect(...)' and `read(...)'
+%%%             functions);</li>
+%%%         <li>sending a list of frames through the CAN port (uses POSIX `write(...)'
+%%%             function);</li>
+%%%         <li>closing the port (via POSIX `close(...)' function).</li>
 %%%      </ul>
 %%%      The driver may be used in <emph>passive</emph> or <emph>active</emph>
 %%%      mode. The passive one gives possibility to recevive frames "on demand"
 %%%      (at the moment of the receiving operation call) with some optional timeout.
 %%%      The active mode ends up with using a thread (OS thread not Erlang process)
 %%%      which listens on the port (with optional timeout) and reads incoming 
-%%%      frames which will be sent to the given process. Both modes may return
-%%%      "raw" or "high-level" frames contents.
+%%%      frames which will be sent to the given process (see {@link listener}).
+%%%      Both modes may return "raw" or "high-level" frames contents.
 %%% @end
 %%% ==========================================================================
 -module ('CAN_drv').
@@ -57,11 +59,12 @@ init () ->
          neg_integer() | {non_neg_integer(), non_neg_integer()}.
 -spec recv (handle(), pos_integer(), non_neg_integer()) -> [{non_neg_integer(), binary()}] | integer().
 -spec listener (handle(), pid(), pos_integer(), pos_integer()) -> pid() | integer().
--spec close (handle()) -> 0 | {error, integer()}.
+-spec close (handle()) -> 0 | neg_integer().
 
 %% @doc Opens CAN port given as the system device path. The driver is expected
 %%      to return "raw" binary frames iff `Mode' is `1' or Erlang lists iff `Mode'
-%%      is `1'.
+%%      is `1'. Returns device handle (an integer) or negative integer indicating
+%%      system error.
 
 open (DevicePath, Raw) when is_list(DevicePath),
                             Raw == 0 orelse Raw == 1 ->
@@ -71,10 +74,13 @@ open (DevicePath, Raw) when is_list(DevicePath),
   end.
 
 %% @doc Sets the port reading and writing baud rate. The rate should be greater than `0'.
+%%      Returns `0' on success or negative integer on failure.
 
 set_baudrate (Handle, BaudRate) when Handle =/= undefined,
                                      BaudRate > 0 ->
   0.
+
+%% @doc Sets the filtering properties. Returns `0' on success or negative integer on failure.
 
 set_filter (Handle, Flags, QueueId, Cob, Id, Mask) 
   when Handle =/= undefined, 
@@ -89,8 +95,9 @@ set_filter (Handle, Flags, QueueId, Cob, Id, Mask)
 %%      The binary given as `Data' should not exceeds 8 bytes.
 %%      The target is a frame id. The returned value may be
 %%      a tuple describing the total number of sent bytes (the 1st item)
-%%      and total sum of data bytes sent (the 2nd item).
-%% @spec (Handle, OutputFrames) -> Status | neg_integer()
+%%      and total sum of data bytes sent (the 2nd item) or negative integer if
+%%      some failure occured.
+%% @spec (Handle, OutputFrames) -> Status | 0 | neg_integer()
 %% where OutputFrames = [{pos_integer(), binary()}],
 %%       Status       = {pos_integer(), pos_integer()}
 
@@ -104,7 +111,11 @@ send (Handle, Ms) when Handle =/= undefined, is_list(Ms) ->
 %%      with the given `Timeout' (the value `0' puts the
 %%      reading operation in non-block mode). The frames
 %%      will be read in packets containing `ChunkSize'
-%%      frames.
+%%      frames. Returns `0' if no data is available yet or
+%%      list of tuples `{Source, Data}' or "non-raw" read mode
+%%      (where `Source' is an id and `Data' is a binary) or
+%%      a binary (possibly huge) in case of "raw" mode containing
+%%      the whole input buffer just read.
 
 recv (Handle, ChunkSize, Timeout) when ChunkSize > 0,
                                        Timeout >= 0-> 
@@ -115,23 +126,31 @@ recv (Handle, ChunkSize, Timeout) when ChunkSize > 0,
 
 %% @doc Closes the port. If a receiving thread has been created
 %%      and still runs it will be stopped and destroyed before
-%%      the port is shut down.
+%%      the port is shut down. Returns `0' on success or negative
+%%      integer indicating specific OS error.
 
 close (Handle) ->
   case Handle of
     <<>> -> 0;
-    _    -> {error, 0}
+    _    -> -1
   end.
 
 %% @doc Puts the driver in <emph>active</emph> mode. A special
 %%      thread is created which tries to read the CAN port with
-%%      the given timeout (see {@link recv}). The received frames
+%%      the given timeout (see {@link recv/3}). The received frames
 %%      are sent to the `Receiver' process in a form specified
-%%      during opening the port (se {@link open}). The function
+%%      during opening the port (see {@link open/2}). The function
 %%      may be called more than once -- only one thread is 
 %%      guaranteed to be created. The result is the previous
 %%      receiver's pid, `0' if the function is called for the 
 %%      1st time or negative integer indicating an error.
+%%     
+%%      The thread sends `{can, DeviceOSDecriptor, Messages}' messages
+%%      to the receiver. `DeviceOSDescriptor' is the OS low-level file
+%%      descriptor of a CAN port, `Messages' is the same as the return
+%%      value of `recv/3' function.
+%%
+%% @see recv/3
 
 listener (Handle, Receiver, ChunkSize, Timeout) when is_pid(Receiver),
                                                      ChunkSize > 0,
@@ -148,7 +167,12 @@ translate_errno (ErrNo) when is_integer(ErrNo) ->
 translate_errno (_) ->
   badarg.
 
-check_tuple (A, B) -> 0.
+%% @private
+
+check_tuple (_A, _B) -> 0.
+
+%% @doc Creates a file containing 256 samples of CAN readings. May be used
+%%      to test the driver.
 
 create_sample_device (Path) ->
   {ok, F} = file:open(Path, [write]),
